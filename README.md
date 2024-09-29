6778946dcdca8b9977969632290d74f9f5434ed0
Pablo Orejuela <porejuelac@gmail.com>
2024-09-29 00:10:00 -0500

    Creado el controlador del Dashboard, ya lista las tareas

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 <h1>App del curso de Laravel 11</h1>
-
 <h3>Gestor de Tareas con Laravel</h3>
 
+<ul>
+    <li>Creado el controlador de el dashboard</li>
+</ul>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,21 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    <h1 style="font-size:2em;color:blueviolet;font-weight:bold;">Bienvenido al gestor de tareas</h1>
+                    {{$user->name}}
+                    <div>
+                        {{$tasks->count()}} tareas registradas
+                        
+                        @foreach ($tasks as $task)
+                            <p class="mt-4 text-lg text-purple-600">
+                                {{$task->title}}:
+                            </p>
+                            <p> 
+                                {{$task->description}}
+                            </p>
+                        @endforeach
+                        
+                    </div>
                 </div>
             </div>
         </div>
